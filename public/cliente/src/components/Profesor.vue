7cb6abdacf0d926781677dc8479125a01c42a619
Rodrigo Iglesias <rniglesias@gmail.com>
2020-07-08 14:15:33 -0300

    Ver en Profesore.vue que esta el arreglo del style para evitar el v-if en los div

--- public/cliente/src/components/Profesor.vue
+++ public/cliente/src/components/Profesor.vue
@@ -1,6 +1,8 @@
 <template>
   <section class="src-components-profesor">
-    <div v-if="estadoButton == true " class="jumbotron mt-3" style=" margin-left: 16.5%;">
+
+    
+    <div class="jumbotron mt-3" :style="cssChequearEstadoButton()">
       <NavbarProfesor
         :dni="this.dni"
         :legajo="this.getMyLegajo"
@@ -119,124 +121,7 @@
 
     </div>
 
-    <div v-else class="jumbotron mt-3" style=" transition-delay: 0.3s;">
-      <NavbarProfesor
-        :dni="this.dni"
-        :legajo="this.getMyLegajo"
-        @estadoButtonNav="mostrarCont($event)"
-      />
 
-      <img src="../../public/logoInstituto.png" width="300" height="100" />
-      <img src="../../public/images1.jpg" width="300" height="100" align="right" />
-      <hr />
-      <br />
-
-      <div v-if="this.getMyData">
-        <h1>Bienvenido, {{this.getMyData.nombre}} {{this.getMyData.apellido}}</h1>
-        <hr />
-      </div>
-
-      Este es el Portal de Profesores, desde la barra lateral podrás gestionar tus cursos, calificar a tus
-      estudiantes, visualizar tus próximas clases o realizar cambios en tu perfil desde el menú de opciones
-
-      <hr>
-
-      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img
-              class="d-block w-100"
-              src="../../public/cimg1.jpg"
-              alt="First slide"
-              width="1000"
-              height="400"
-            />
-          </div>
-          <div class="carousel-item">
-            <img
-              class="d-block w-100"
-              src="../../public/cimg2.jpg"
-              alt="Second slide"
-              width="1000"
-              height="400"
-            />
-          </div>
-          <div class="carousel-item">
-            <img
-              class="d-block w-100"
-              src="../../public/cimg3.jpg"
-              alt="Third slide"
-              width="1000"
-              height="400"
-            />
-          </div>
-        </div>
-        <a
-          class="carousel-control-prev"
-          href="#carouselExampleIndicators"
-          role="button"
-          data-slide="prev"
-        >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a
-          class="carousel-control-next"
-          href="#carouselExampleIndicators"
-          role="button"
-          data-slide="next"
-        >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div>
-
-      <hr>
-           
-      <h4>Lista de todos los profesores del Instituto (estos son tus compañeros)</h4>
-
-      <div v-if="this.getArrayProfesores.length">
-        <table class="table">
-          <tr class="titulo">
-            <th>Nombre</th>
-            <th>Apellido</th>
-            <th>Email</th>
-          </tr>
-          <tr
-            v-for="(profesor,index) in this.getArrayProfesores"
-            :key="index"
-            :class="cssEvento(index)"
-          >
-            <td>{{ profesor.nombre }}</td>
-            <td>{{ profesor.apellido }}</td>
-            <td>{{ profesor.email }}</td>
-          </tr>
-        </table>
-      </div>
-      <div v-else class="alert alert-danger">No se encontraron profesores</div>
-
-      <br />
-      <br />
-      <br />
-      <hr>
-
-      <footer id="sticky-footer" class="py-4 bg-dark text-white-50">
-        <div class="container text-center">
-          <a class="btn btn-block btn-social btn-twitter">
-            <img src="../../public/Twitter.png" width="40" height="40" border="2" hspace="4" />
-            <img src="../../public/Gmail.png" width="40" height="40" border="2" hspace="4" />
-            <img src="../../public/Facebook.png" width="40" height="40" border="2" hspace="4" />
-          </a>
-          <small>Copyright &copy; Your Website</small>
-        </div>
-      </footer>
-
-    </div>
   </section>
 </template>
 
@@ -253,11 +138,6 @@
     props: ['dni'],
     mounted () {
       this.$store.dispatch('actionCargarArrayProfesores')
-      //console.log(this.$store.state.arrayProfesores)
-
-      //this.misDatos = this.getMyData()
-
-
     },
     data () {
       return {
@@ -267,11 +147,19 @@
     },
     methods: {
       cssEvento(index) {
-          //Este return de un objeto es prque vue asi lo requiere, pero ponerlo acá queda mucho mas entendible en el codigo HTML.
+          // Este return de un objeto es prque vue asi lo requiere, 
+          // pero ponerlo acá queda mucho mas entendible en el codigo HTML.
           return {
               'fila-impar': (index % 2) == 0,
               'fila-par': (index % 2) != 0
           }
+        },
+
+        cssChequearEstadoButton() {
+          if (this.estadoButton)
+            return "margin-left: 16.5%;"
+          else
+            return "transition-delay: 0.3s;"
         },
 
         mostrarCont(estado) {
